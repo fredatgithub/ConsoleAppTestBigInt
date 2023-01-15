e923f7e4747f062b2ea1c2d30bc5e580c104f145
fred <domaine75@gmail.com>
2023-01-16 00:30:37 +0100

    Ajoutez des fichiers projet.

---
+++ ConsoleAppBigInt/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Numerics;
+
+Console.WriteLine("Hello, big integer");
+var number = ulong.MaxValue;
+BigInteger aNumber = new BigInteger(number);
+Console.WriteLine($"ulong max value is {aNumber}");
+Console.WriteLine("Calcul des nombres premiers à partir de ulong.MaxValue :");
+for (int i = 1;i < 10; i++)
+{
+  BigInteger bNumber = new BigInteger(number) + new BigInteger(i);
+  if (IsPrime(bNumber))
+  {
+    Console.WriteLine($"{bNumber} is prime");
+  }
+  else 
+  {
+    Console.WriteLine($"{bNumber} is not a prime");
+  }
+}
+
+bool IsPrime(BigInteger bNumber)
+{
+  if (bNumber == 0 || bNumber == 1)
+  {
+    return false;
+  }
+
+  if (bNumber == 2)
+  {
+    return true;
+  }
+
+  if (bNumber == 3)
+  {
+    return true;
+  }
+
+  if (bNumber % 2 == 0 )
+  {
+    return false;
+  }
+  if (bNumber == 5 )
+  {
+    return true;
+  }
+
+
+}
+
+Console.WriteLine("press any key to exit:");
+Console.ReadKey();
+
---
+++ ConsoleAppTestBigInt/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+//using System.Numerics; // .NET7
+
+namespace ConsoleAppTestBigInt
+{
+  internal class Program
+  {
+    static void Main(string[] arguments)
+    {
+      var number = ulong.MaxValue;
+      Console.WriteLine(number);
+      Console.WriteLine(string.Format("###,###", number));
+      string numberInString = number.ToString();
+      Console.WriteLine(numberInString);
+
+      Console.WriteLine("press any key to exit:");
+      Console.ReadKey();
+    }
+
+    private bool IsEven(ulong number)
+    {
+      return number % 2 == 0;
+    }
+  }
+}
